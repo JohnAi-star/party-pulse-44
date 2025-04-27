d1757e173fd1ac5c063024c2460f15f22b222b41
JohnAi-star <businessjohn460@gmail.com>
2025-04-27 18:57:41 +0200

    updated lib/constants.ts

--- lib/constants.ts
+++ lib/constants.ts
@@ -5,7 +5,7 @@ export const MOCK_ACTIVITIES = [
     description: 'Learn how to make delicious cocktails from expert mixologists. Perfect for hen parties, birthdays or corporate events.',
     city: 'London',
     priceFrom: 29,
-    image: 'https://images.pexels.com/photos/1283219/pexels-photo-1283219.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+    image: 'https://images.pexels.com/photos/2531186/pexels-photo-2531186.jpeg',
     category: 'Hen Do',
     rating: 4.8,
     duration: '2 hours',
@@ -17,7 +17,7 @@ export const MOCK_ACTIVITIES = [
     description: 'Sample the finest craft beers with our guided brewery tour. Great for stag dos and team building events.',
     city: 'Manchester',
     priceFrom: 35,
-    image: 'https://images.pexels.com/photos/1269025/pexels-photo-1269025.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+    image: 'https://images.pexels.com/photos/5530171/pexels-photo-5530171.jpeg',
     category: 'Stag Do',
     rating: 4.6,
     duration: '3 hours',
@@ -29,7 +29,7 @@ export const MOCK_ACTIVITIES = [
     description: 'Test your problem-solving skills in our themed escape rooms. Perfect for team building and birthday parties.',
     city: 'Birmingham',
     priceFrom: 22,
-    image: 'https://images.pexels.com/photos/8111311/pexels-photo-8111311.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+    image: 'https://images.pexels.com/photos/4019766/pexels-photo-4019766.jpeg',
     category: 'Team Building',
     rating: 4.9,
     duration: '1 hour',
@@ -41,7 +41,7 @@ export const MOCK_ACTIVITIES = [
     description: 'Get creative with clay in our hands-on pottery workshop. Ideal for hen parties and special celebrations.',
     city: 'Leeds',
     priceFrom: 45,
-    image: 'https://images.pexels.com/photos/3094341/pexels-photo-3094341.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+    image: 'https://images.pexels.com/photos/7262997/pexels-photo-7262997.jpeg',
     category: 'Hen Do',
     rating: 4.7,
     duration: '2.5 hours',
@@ -53,7 +53,7 @@ export const MOCK_ACTIVITIES = [
     description: 'Release your inner viking with our exciting axe throwing activity. Perfect for stag dos and team bonding.',
     city: 'Liverpool',
     priceFrom: 25,
-    image: 'https://images.pexels.com/photos/8111298/pexels-photo-8111298.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+    image: 'https://images.pexels.com/photos/8111298/pexels-photo-8111298.jpeg',
     category: 'Stag Do',
     rating: 4.5,
     duration: '1.5 hours',
@@ -65,7 +65,7 @@ export const MOCK_ACTIVITIES = [
     description: 'Learn the art of sushi making from professional chefs. Great for birthdays and corporate events.',
     city: 'Edinburgh',
     priceFrom: 40,
-    image: 'https://images.pexels.com/photos/2098143/pexels-photo-2098143.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+    image: 'https://images.pexels.com/photos/8951999/pexels-photo-8951999.jpeg',
     category: 'Birthday',
     rating: 4.8,
     duration: '2 hours',
@@ -77,7 +77,7 @@ export const MOCK_ACTIVITIES = [
     description: 'Sample premium wines with expert guidance. Perfect for sophisticated hen parties and celebrations.',
     city: 'Bristol',
     priceFrom: 38,
-    image: 'https://images.pexels.com/photos/1479706/pexels-photo-1479706.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+    image: 'https://images.pexels.com/photos/5946975/pexels-photo-5946975.jpeg',
     category: 'Hen Do',
     rating: 4.7,
     duration: '2 hours',
@@ -89,7 +89,7 @@ export const MOCK_ACTIVITIES = [
     description: 'Race against friends on our professional indoor track. Ideal for stag dos and team events.',
     city: 'Newcastle',
     priceFrom: 50,
-    image: 'https://images.pexels.com/photos/12920/pexels-photo-12920.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+    image: 'https://images.pexels.com/photos/9827179/pexels-photo-9827179.jpeg',
     category: 'Stag Do',
     rating: 4.9,
     duration: '2 hours',
@@ -103,42 +103,42 @@ export const CATEGORIES = [
     title: 'Hen Do',
     icon: 'champagne',
     description: 'Unforgettable hen party activities and experiences',
-    image: 'https://images.pexels.com/photos/3434554/pexels-photo-3434554.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'
+    image: 'https://images.pexels.com/photos/3171837/pexels-photo-3171837.jpeg'
   },
   {
     id: 'stag-do',
     title: 'Stag Do',
     icon: 'beer',
     description: 'Epic stag do activities and adventures',
-    image: 'https://images.pexels.com/photos/5531553/pexels-photo-5531553.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'
+    image: 'https://images.pexels.com/photos/4255483/pexels-photo-4255483.jpeg'
   },
   {
     id: 'team-building',
     title: 'Team Building',
     icon: 'users',
     description: 'Boost team morale with exciting group activities',
-    image: 'https://images.pexels.com/photos/3184360/pexels-photo-3184360.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'
+    image: 'https://images.pexels.com/photos/3184291/pexels-photo-3184291.jpeg'
   },
   {
     id: 'birthday',
     title: 'Birthday',
     icon: 'cake',
     description: 'Make your birthday celebration truly special',
-    image: 'https://images.pexels.com/photos/2291347/pexels-photo-2291347.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'
+    image: 'https://images.pexels.com/photos/7180617/pexels-photo-7180617.jpeg'
   },
   {
     id: 'corporate',
     title: 'Corporate',
     icon: 'briefcase',
     description: 'Impress clients and colleagues with premium experiences',
-    image: 'https://images.pexels.com/photos/3184339/pexels-photo-3184339.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'
+    image: 'https://images.pexels.com/photos/3183150/pexels-photo-3183150.jpeg'
   },
   {
     id: 'christmas',
     title: 'Christmas',
     icon: 'snowflake',
     description: 'Festive activities to celebrate the holiday season',
-    image: 'https://images.pexels.com/photos/716658/pexels-photo-716658.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'
+    image: 'https://images.pexels.com/photos/1708601/pexels-photo-1708601.jpeg'
   },
 ];
 
@@ -146,56 +146,56 @@ export const CITIES = [
   {
     id: 'london',
     name: 'London',
-    image: 'https://images.pexels.com/photos/460672/pexels-photo-460672.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+    image: 'https://images.pexels.com/photos/460672/pexels-photo-460672.jpeg',
     description: 'Discover exciting group activities in the heart of the capital',
     activities: 150
   },
   {
     id: 'manchester',
     name: 'Manchester',
-    image: 'https://images.pexels.com/photos/1942321/pexels-photo-1942321.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+    image: 'https://images.pexels.com/photos/1942321/pexels-photo-1942321.jpeg',
     description: 'From cocktail making to escape rooms, Manchester has it all',
     activities: 89
   },
   {
     id: 'birmingham',
     name: 'Birmingham',
-    image: 'https://images.pexels.com/photos/6045083/pexels-photo-6045083.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+    image: 'https://images.pexels.com/photos/6045083/pexels-photo-6045083.jpeg',
     description: 'The perfect destination for unforgettable group experiences',
     activities: 76
   },
   {
     id: 'edinburgh',
     name: 'Edinburgh',
-    image: 'https://images.pexels.com/photos/1144199/pexels-photo-1144199.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+    image: 'https://images.pexels.com/photos/1144199/pexels-photo-1144199.jpeg',
     description: 'Historic charm meets modern entertainment in Edinburgh',
     activities: 64
   },
   {
     id: 'bristol',
     name: 'Bristol',
-    image: 'https://images.pexels.com/photos/1058958/pexels-photo-1058958.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+    image: 'https://images.pexels.com/photos/1058958/pexels-photo-1058958.jpeg',
     description: 'Creative activities in the South West\'s most vibrant city',
     activities: 55
   },
   {
     id: 'liverpool',
     name: 'Liverpool',
-    image: 'https://images.pexels.com/photos/552766/pexels-photo-552766.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+    image: 'https://images.pexels.com/photos/552766/pexels-photo-552766.jpeg',
     description: 'Experience the best group activities in the cultural capital',
     activities: 72
   },
   {
     id: 'leeds',
     name: 'Leeds',
-    image: 'https://images.pexels.com/photos/6488512/pexels-photo-6488512.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+    image: 'https://images.pexels.com/photos/6488512/pexels-photo-6488512.jpeg',
     description: 'Yorkshire\'s finest selection of group experiences',
     activities: 48
   },
   {
     id: 'newcastle',
     name: 'Newcastle',
-    image: 'https://images.pexels.com/photos/2662792/pexels-photo-2662792.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+    image: 'https://images.pexels.com/photos/2662792/pexels-photo-2662792.jpeg',
     description: 'The North East\'s top destination for celebrations',
     activities: 52
   },
@@ -206,7 +206,7 @@ export const BLOG_POSTS = [
     id: '1',
     title: '10 Unique Hen Party Ideas That Break The Mold',
     snippet: 'Move beyond the traditional with these creative hen party activities that will make your celebration truly memorable.',
-    image: 'https://images.pexels.com/photos/3171837/pexels-photo-3171837.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+    image: 'https://images.pexels.com/photos/3171837/pexels-photo-3171837.jpeg',
     author: 'Sarah Johnson',
     date: '2024-03-15',
     category: 'Party Planning',
@@ -216,7 +216,7 @@ export const BLOG_POSTS = [
     id: '2',
     title: 'The Ultimate Guide to Planning a Corporate Team Building Day',
     snippet: 'Boost team morale and productivity with these engaging activities that will bring your colleagues closer together.',
-    image: 'https://images.pexels.com/photos/3184291/pexels-photo-3184291.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+    image: 'https://images.pexels.com/photos/3184291/pexels-photo-3184291.jpeg',
     author: 'Michael Chen',
     date: '2024-03-10',
     category: 'Corporate Events',
@@ -226,7 +226,7 @@ export const BLOG_POSTS = [
     id: '3',
     title: 'Budget-Friendly Birthday Party Ideas That Look Expensive',
     snippet: 'Create an unforgettable birthday celebration without breaking the bank with these smart planning tips.',
-    image: 'https://images.pexels.com/photos/7180617/pexels-photo-7180617.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+    image: 'https://images.pexels.com/photos/7180617/pexels-photo-7180617.jpeg',
     author: 'Emma Williams',
     date: '2024-03-05',
     category: 'Party Planning',
@@ -236,7 +236,7 @@ export const BLOG_POSTS = [
     id: '4',
     title: 'Seasonal Activities: Making the Most of Summer Celebrations',
     snippet: 'From outdoor adventures to rooftop parties, discover the best ways to celebrate during the warmer months.',
-    image: 'https://images.pexels.com/photos/5638/beach-sunset-party-summer.jpg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+    image: 'https://images.pexels.com/photos/5530176/pexels-photo-5530176.jpeg',
     author: 'Tom Parker',
     date: '2024-02-28',
     category: 'Seasonal',
@@ -248,19 +248,19 @@ export const TEAM_MEMBERS = [
   {
     name: 'Emily Thompson',
     role: 'Founder & CEO',
-    image: 'https://images.pexels.com/photos/3796217/pexels-photo-3796217.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+    image: 'https://images.pexels.com/photos/3796217/pexels-photo-3796217.jpeg',
     bio: 'With over 15 years in event planning, Emily founded Party Pulse to help people create unforgettable celebrations.'
   },
   {
     name: 'James Wilson',
     role: 'Head of Experiences',
-    image: 'https://images.pexels.com/photos/2379004/pexels-photo-2379004.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+    image: 'https://images.pexels.com/photos/2379004/pexels-photo-2379004.jpeg',
     bio: 'James curates our activity selection, ensuring only the highest quality experiences make it onto our platform.'
   },
   {
     name: 'Sophie Chen',
     role: 'Customer Success Manager',
-    image: 'https://images.pexels.com/photos/3760263/pexels-photo-3760263.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+    image: 'https://images.pexels.com/photos/3760263/pexels-photo-3760263.jpeg',
     bio: 'Sophie leads our customer support team, making sure every booking runs smoothly from start to finish.'
   }
 ];
@@ -269,21 +269,21 @@ export const TESTIMONIALS = [
   {
     name: 'Rachel B.',
     location: 'Manchester',
-    image: 'https://images.pexels.com/photos/1181686/pexels-photo-1181686.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+    image: 'https://images.pexels.com/photos/1181686/pexels-photo-1181686.jpeg',
     text: 'Party Pulse made organizing my sister\'s hen do so easy! The cocktail making class was a huge hit, and the booking process was seamless.',
     rating: 5
   },
   {
     name: 'Mark T.',
     location: 'London',
-    image: 'https://images.pexels.com/photos/1516680/pexels-photo-1516680.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+    image: 'https://images.pexels.com/photos/1516680/pexels-photo-1516680.jpeg',
     text: 'We used Party Pulse for our team building day and couldn\'t be happier. The escape room challenge was perfect for bringing the team together.',
     rating: 5
   },
   {
     name: 'Sarah W.',
     location: 'Edinburgh',
-    image: 'https://images.pexels.com/photos/1181695/pexels-photo-1181695.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+    image: 'https://images.pexels.com/photos/1181695/pexels-photo-1181695.jpeg',
     text: 'Found the perfect birthday activity through Party Pulse. The wine tasting experience was sophisticated and fun - exactly what I wanted!',
     rating: 5
   }
