95d333d3f70197a22537f97c914206c0a90ec6fc
JohnAi-star <businessjohn460@gmail.com>
2025-05-02 20:32:19 +0200

    updated app/sign-in/[[...sign-in]]/page.tsx

--- app/sign-in/[[...sign-in]]/page.tsx
+++ app/sign-in/[[...sign-in]]/page.tsx
@@ -1,6 +1,4 @@
-// app/sign-in/[[...sign-in]]/page.tsx
 import { SignIn } from "@clerk/nextjs";
-
 export default function Page() {
   return (
     <div className="flex min-h-[calc(100vh-4rem)] items-center justify-center">
