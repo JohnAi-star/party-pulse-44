bab63bab26fe804ff9d1f2c51cc79c4f9560e3f7
JohnAi-star <businessjohn460@gmail.com>
2025-05-03 10:31:38 +0200

    updated next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
  /** @type {import('next').NextConfig} */ const nextConfig = {
+  output: 'standalone',
   eslint: {
     ignoreDuringBuilds: true,
+  },
+  reactStrictMode: true,
+  experimental: {
+    appDir: true,
   },
   images: {
     unoptimized: true,
