c6251827701cd7d65dd7370facb398a2d5a2a6fc
JohnAi-star <businessjohn460@gmail.com>
2025-05-03 11:02:00 +0200

    updated next.config.js

--- next.config.js
+++ next.config.js
@@ -1,26 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: { 
-    unoptimized: true,
-    domains: ['images.pexels.com']
-  },
   eslint: {
     ignoreDuringBuilds: true,
   },
-  trailingSlash: true,
-  // Add static routes that don't need dynamic params
-  exportPathMap: async function() {
-    return {
-      '/': { page: '/' },
-      '/activities': { page: '/activities' },
-      '/about': { page: '/about' },
-      '/blog': { page: '/blog' },
-      '/contact': { page: '/contact' },
-      '/locations': { page: '/locations' },
-      '/sign-in': { page: '/sign-in/[[...sign-in]]' },
-      '/sign-up': { page: '/sign-up/[[...sign-up]]' },
-    };
+  images: {
+    unoptimized: true,
+    domains: ['images.pexels.com'],
   },
+  trailingSlash: true,
 };
 
 module.exports = nextConfig;
