5f0ee9110e8321339022c8f43b1e59fbd229c5dd
JohnAi-star <businessjohn460@gmail.com>
2025-05-03 10:59:20 +0200

    updated next.config.js

--- next.config.js
+++ next.config.js
@@ -7,7 +7,6 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
-  output: 'export',
   trailingSlash: true,
   // Add static routes that don't need dynamic params
   exportPathMap: async function() {
